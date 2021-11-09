668b36d48abb1acd38190db7855a4e199bc59ea3
YRKWON <feelstyo@gmail.com>
2021-11-09 17:48:38 +0900

    업뎃

--- portfolio/src/main/java/com/yr/controller/portfolioController.java
+++ portfolio/src/main/java/com/yr/controller/portfolioController.java
@@ -36,32 +36,4 @@ public class portfolioController {
 		return "redirect:/index";
 	}
 	
-	@RequestMapping("/download")
-	public void download(Model d, HttpServletRequest request, HttpServletResponse response)
-			throws Exception {
-		String fname = "cv.pdf";
-		File file = new File("uploads/"+fname);
-		System.out.println("## viewer오신 것을 환영합니다##");
-		System.out.println("전체파일명:"+file.getPath());
-		System.out.println("파일명:"+file.getName());
-		System.out.println("파일길이:"+(int)file.length());
-
-		response.setContentType("application/download; charset=UTF-8");
-
-		response.setContentLength((int)file.length());
-
-		fname = URLEncoder.encode(fname,"utf-8").replaceAll("\\+", " ");
-
-		response.setHeader("Content-Disposition",
-				"attachment;filename=\""+fname+"\"");
-		response.setHeader("Content-Transfer-Encoding", "binary");
-
-		FileInputStream fis = new FileInputStream(file);
-		
-		OutputStream out = response.getOutputStream();
-
-		FileCopyUtils.copy(fis, out);
-
-		out.flush();
-	}
 }
